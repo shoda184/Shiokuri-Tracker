1505870a63ee4b489fb46c9c10f161f87a095af9
shoda184 <160000477+shoda184@users.noreply.github.com>
2025-08-14 17:14:39 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/shiokuri-tracker/", // あなたのリポジトリ名に置き換えてください
-})
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
